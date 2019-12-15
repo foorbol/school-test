04a51cfb6e4d7489a93b4820141a2c968b03d291
Pavel Shalamkov <commercialsuiside@gmail.com>
2019-12-15 20:56:42 +0300

    ADDED: Classes + attributes

--- main.js
+++ main.js
@@ -6,27 +6,36 @@ const form = document.getElementById('form');
 console.warn(form);
 
 CONFIG
-  .forEach((item) => {
+  .forEach((item, x) => {
     const wrapper = document.createElement('div');
-    wrapper.classList.add('form-check');
+    wrapper.classList.add('wrapper');
 
     const heading = document.createElement('h2');
     heading.textContent = item.heading;
     wrapper.appendChild(heading);
 
-    item.wrongAnswers.forEach((answer) => {
-      const optionContainer = document.createElement('div');
-      const input = document.createElement('input');
-      const label = document.createElement('label');
-      label.classList.add('form-check-label');
+    item.wrongAnswers
+      .forEach((answer, y) => {
+        const id = `${x}-${y}`;
+        const optionContainer = document.createElement('div');
+        optionContainer.classList.add('form-check');
+        const input = document.createElement('input');
+        input.classList.add('form-check-input');
+        input.setAttribute('id', id);
+        input.setAttribute('name', x.toString());
+        input.setAttribute('value', answer);
+        const label = document.createElement('label');
+        label.setAttribute('for', id);
+        label.textContent = answer;
+        label.classList.add('form-check-label');
 
-      input.setAttribute('type', 'radio');
+        input.setAttribute('type', 'radio');
 
-      optionContainer.appendChild(input);
-      optionContainer.appendChild(label);
+        optionContainer.appendChild(input);
+        optionContainer.appendChild(label);
 
-      wrapper.appendChild(optionContainer);
-    });
+        wrapper.appendChild(optionContainer);
+      });
 
     form.appendChild(wrapper);
     // console.warn(heading);

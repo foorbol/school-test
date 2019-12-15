030239a5c1387defb4ad8e4d51196162f71cdbc4
Pavel Shalamkov <commercialsuiside@gmail.com>
2019-12-15 16:03:10 +0300

    ADDED: Basic HTML communication

--- main.js
+++ main.js
@@ -2,4 +2,13 @@
  * Main logic
  */
 
-console.warn(CONFIG);
+const container = document.getElementById('container');
+console.warn(container);
+
+CONFIG
+  .forEach((item, i) => {
+    const heading = document.createElement('h1');
+    heading.textContent = item.heading;
+    container.appendChild(heading);
+    console.warn(heading);
+  });

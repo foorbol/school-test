0e22401cbd296fbf3df8117044d84b9e551cb76b
Pavel Shalamkov <commercialsuiside@gmail.com>
2019-12-15 15:35:09 +0300

    ADDED: Main and config files

---
+++ config.js
@@ -0,0 +1,24 @@
+/**
+ * Config file
+ */
+
+const CONFIG = [
+  {
+    heading: 'Question one',
+    correctAnswer: 'Correct answer',
+    wrongAnswers: [
+      'Wrong answer 1',
+      'Wrong answer 2',
+      'Wrong answer 3'
+    ]
+  },
+  {
+    heading: 'Question two',
+    correctAnswer: 'Correct answer',
+    wrongAnswers: [
+      'Wrong answer 1',
+      'Wrong answer 2',
+      'Wrong answer 3'
+    ]
+  }
+];
---
+++ main.js
@@ -0,0 +1,5 @@
+/**
+ * Main logic
+ */
+
+console.warn(CONFIG);
